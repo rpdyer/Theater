b54e5a3e892bf36ae020590feda3dc9c9e6acc65
Robert Dyer <rpdyer@hotmail.com>
2016-04-06 16:50:44 -0600

    Corrected the last checkin -- "B10" rather than "B1" should be the expected result.

--- TheaterSeating/TheaterSeating/SeatFinderTests.cs
+++ TheaterSeating/TheaterSeating/SeatFinderTests.cs
@@ -33,10 +33,10 @@ namespace TheaterSeating
                   "D2,D3,D4,D5,D6,D7,D8,", +
                    2, Result = "D9,D10")]
         [TestCase("A1,A2,A3,A4,A5,A6,A7,A8,A9,A10," +
-                  "B2,B3,B4,B5,B6,B7,B8,B9,B10," +
+                  "B1,B2,B3,B4,B5,B6,B7,B8,B9," +
                   "C1,C2,C3,C4,C5,C6,C7,C8,C9,C10," +
                   "D2,D3,D4,D5,D6,D7,D8,", +
-                   1, Result = "B1")]
+                   1, Result = "B10")]
         public string SuggestTest1(string bookedSeatsStr, int partySize)
         {
             SeatFinder seatFinder = new SeatFinder(bookedSeatsStr);
