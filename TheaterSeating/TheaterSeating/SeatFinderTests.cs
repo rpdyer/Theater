d92118b9c9b69b100374cbb346fcb6cb3aed95f4
Robert Dyer <rpdyer@hotmail.com>
2016-04-06 16:48:52 -0600

    Added another test case.
    This tests a single seat suggestion that expects to have seat "B10"
    suggested.

--- TheaterSeating/TheaterSeating/SeatFinderTests.cs
+++ TheaterSeating/TheaterSeating/SeatFinderTests.cs
@@ -32,6 +32,11 @@ namespace TheaterSeating
                   "C1,C2,C3,C4,C5,C6,C7,C8,C9,C10," +
                   "D2,D3,D4,D5,D6,D7,D8,", +
                    2, Result = "D9,D10")]
+        [TestCase("A1,A2,A3,A4,A5,A6,A7,A8,A9,A10," +
+                  "B2,B3,B4,B5,B6,B7,B8,B9,B10," +
+                  "C1,C2,C3,C4,C5,C6,C7,C8,C9,C10," +
+                  "D2,D3,D4,D5,D6,D7,D8,", +
+                   1, Result = "B1")]
         public string SuggestTest1(string bookedSeatsStr, int partySize)
         {
             SeatFinder seatFinder = new SeatFinder(bookedSeatsStr);
