61d9846f6ca7e78d953d1ad6eb9a0951347d5a35
Robert Dyer <rpdyer@hotmail.com>
2016-04-11 09:29:38 -0600

    Refactored the FindAdjacentSeats method

--- TheaterSeating/TheaterSeating/SeatFinder.cs
+++ TheaterSeating/TheaterSeating/SeatFinder.cs
@@ -46,7 +46,122 @@ namespace TheaterSeating
         public List<Seat> Suggest(int partySize)
         {
             List<Seat> ret = null;
+            foreach (var row in Theater.Rows)
+            {
+                ret = FindAdjacentSeats(row, Theater.Seats[row], partySize);
+                if (ret != null)
+                    break;
+            }
             return ret;
+        }
+
+        public List<Seat> FindAdjacentSeats(string row, List<int> seats, int partySize)
+        {
+            List<Seat> ret = null;
+            var adjacentSeats = FindAdjacentSeatsInMiddle(row, seats, partySize);
+            if (adjacentSeats == null)
+            {
+                adjacentSeats = FindAdjacentSeatsOnLeft(row, seats, partySize);
+                if (adjacentSeats == null)
+                    adjacentSeats = FindAdjacentSeatsOnRight(row, seats, partySize);
+            }
+            if (adjacentSeats != null)
+            {
+                ret = new List<Seat>();
+                for (int i = adjacentSeats.Item1; i <= adjacentSeats.Item2; i++)
+                {
+                    ret.Add(new Seat(row, i));
+                }
+            }
+            return ret;
+
+        }
+
+        public Tuple<int,int> FindAdjacentSeatsInMiddle(string row, List<int> seats, int partySize)
+        {
+            // first try to find seats in the middle:
+            int dec = partySize / 2;
+            if (dec > 0)
+                dec--;
+            int firstSeat = (seats.Count / 2) - dec;
+            int lastSeat = firstSeat + partySize - 1;
+            // confirm if all are available:
+            bool ok = true;
+            for (int i = firstSeat; i <= lastSeat && ok; i++)
+            {
+                ok = !BookedSeats.Contains(row + i);
+            }
+            return ok ? new Tuple<int, int>(firstSeat, lastSeat) : null;
+        }
+
+        /// <summary>
+        /// Count how many seats are available at the left.
+        /// Precondition: At least one seat in the middle is booked.
+        /// </summary>
+        /// <param name="row"></param>
+        /// <param name="seats"></param>
+        /// <param name="partySize"></param>
+        /// <returns></returns>
+        public Tuple<int, int> FindAdjacentSeatsOnLeft(string row, List<int> seats, int partySize)
+        {
+            int firstSeat = -1;
+            int lastSeat = -1;
+            bool ok = false;
+            int count = 0;
+            for (int i = 0; i < seats.Count / 2; i++)
+            {
+                if (!BookedSeats.Contains(row + seats[i]))
+                    count++;
+                else
+                {
+                    if (count >= partySize)
+                    {
+                        int index = i - 1;
+                        lastSeat = seats[index];
+                        index = index - partySize + 1;
+                        firstSeat = seats[index];
+                        // avoid leaving an orphaned empty seat on the far left:
+                        ok = index != 1;
+                    }
+                    break;
+                }
+            }
+            return ok ? new Tuple<int, int>(firstSeat, lastSeat) : null;
+        }
+
+        /// <summary>
+        /// Count how many seats are available at the right:
+        /// Precondition: At least one seat in the middle is booked.
+        /// </summary>
+        /// <param name="row"></param>
+        /// <param name="seats"></param>
+        /// <param name="partySize"></param>
+        /// <returns></returns>
+        public Tuple<int, int> FindAdjacentSeatsOnRight(string row, List<int> seats, int partySize)
+        {
+            int firstSeat = -1;
+            int lastSeat = -1;
+            bool ok = false;
+            int count = 0;
+            for (int i = seats.Count - 1; i >= seats.Count / 2; i--)
+            {
+                if (!BookedSeats.Contains(row + seats[i]))
+                    count++;
+                else
+                {
+                    if (count >= partySize)
+                    {
+                        int index = i + 1;
+                        firstSeat = seats[index];
+                        index = index + partySize - 1;
+                        lastSeat = seats[index];
+                        // avoid leaving an orphaned empty seat on the far right:
+                        ok = index != seats.Count - 2;
+                    }
+                    break;
+                }
+            }
+            return ok ? new Tuple<int, int>(firstSeat, lastSeat) : null;
         }
 
     }
--- TheaterSeating/TheaterSeating/SeatFinderTests.cs
+++ TheaterSeating/TheaterSeating/SeatFinderTests.cs
@@ -15,6 +15,8 @@ namespace TheaterSeating
         [TestCase("", 2, Result = "A5,A6")]
         [TestCase("A5,A6", 2, Result = "A3,A4")]
         [TestCase("A5,A6", 3, Result = "B5,B6,B7")]
+        [TestCase("A5,A6", 4, Result = "A1,A2,A3,A4")]
+        [TestCase("A1,A2,A3,A4,A5,A6", 4, Result = "A7,A8,A9,A10")]
         [TestCase("A1,A2,A3,A4,A5,A6,A7,A8,A9,A10," +
                   "B1,B2,B3,B4,B5,B6,B7,B8,B9,B10," +
                   "C1,C2,C3,C4,C5,C6,C7,C8,C9,C10," +
